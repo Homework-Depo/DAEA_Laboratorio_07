c0ec0f63a6837ea772f765bf4f1aa36b4d975055
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-10-10 21:54:24 -0500

    Agregar archivos de proyecto.

---
+++ Business/BProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Data;
+
+namespace Business
+{
+    public class BProduct
+    {
+        public List<Product> GetByName(string Name)
+        {
+            List<Product> result = new List<Product>();
+            DProduct data = new DProduct();
+            var products = data.Get();
+
+            foreach (var product in products)
+            {
+                if (product.Name == Name)
+                {
+                    result.Add(product);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Data/DProduct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entity;
+using System.Data;
+
+namespace Data
+{
+    public class DProduct
+    {
+        public static string connectionString = "Data Source=LAB1504-32\\SQLEXPRESS;Initial Catalog=master;User ID=sa;Password=123456";
+        public List<Product> Get()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                
+                using (SqlCommand command = new SqlCommand("listarProductos", connection)) 
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                ProductId = reader.GetInt32(reader.GetOrdinal("product_id")),
+                                Name = reader.GetString(reader.GetOrdinal("name")),
+                                Price = reader.GetDecimal(reader.GetOrdinal("price")),
+                                Stock = reader.GetInt32(reader.GetOrdinal("stock")),
+                                Active = reader.GetBoolean(reader.GetOrdinal("active"))
+                            };
+                            products.Add(product);
+                        }
+                    }
+
+                }
+                connection.Close();
+            }
+            return products;
+        }
+    }
+}
